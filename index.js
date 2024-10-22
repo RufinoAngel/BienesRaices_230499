bdfbc17766a911043ed370df53ee3ebb43c609e2
Angel de Jesus Rufino Mendoza <230499@utxicotepec.edu.mx>
2024-10-22 13:41:01 -0600

    Practica 09
    Actualización de estandar de tipado para código de NodeJS

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const express= require('express'); //Importar la librería para crear un servidor web
+//ECMA SCRIPT 6
+// CommonJS
+import express from 'express';
+//const express= require('express'); //Importar la librería para crear un servidor web
 
 // instanciar nuestra aplicacion web
 const app=express();
@@ -12,7 +15,8 @@ app.listen(port, ()=>{
 app.get('/',function(req,res){
     res.send("Hola Mundo desde Node, a través del Navegador")
 })
-
+//Routing- enrutamiento .
+//Probamos las rutas para poder presentar mensajes al
 app.get('/QuienSoy',function(req,res){
     res.json({"estudiante":"Angel de Jesus Rufino Mendoza",
         "carrera":"TI DSM",
